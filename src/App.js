db7ce50a484344205b79e816db123b0b19b169f4
Nataliiv <nataliya1986kaduk@gmail.com>
2022-09-22 17:11:55 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by Nataliia Ivanchak and is {""}
-        <a href="https://github.com/Nataliiv/react-weather-app">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by Nataliia Ivanchak and is {""}
+          <a href="https://github.com/Nataliiv/react-weather-app">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
