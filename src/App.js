71ac7201876cb8a0a4935475211cf1e654bd66cf
Nataliiv <nataliya1986kaduk@gmail.com>
2022-09-27 17:00:01 +0300

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           This project was coded by Nataliia Ivanchak and is {""}
           <a href="https://github.com/Nataliiv/react-weather-app">
